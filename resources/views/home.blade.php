4e9174d6c8bfa8c8dacfd575bb0ce64384d8f135
AhmadJulianda <ahmadjulianda@gmail.com>
2025-07-14 13:46:53 +0700

    Develop home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="{{ asset('style.css') }}">
+  <title>Home</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="main">
+      <div class="task-summary-container">
+       <h1 class="task-summary-heading">Publication Dashboard Data 2025/2026</h1>
+       <h2 class="task-summary-body">Realtime Pendataan Sekolah Yayasan Pendidikan Iskandar Muda Wahdah Islamiyah</h2>
+
+      <div  class="task-summary-list">
+        <span class="material-icons">check_circle</span>
+        <h2>You have completed 1 task</h2>
+      </div>
+
+      <div class="task-summary-list">
+        <span class="material-icons">list</span>
+        <h2>You still have 5 tasks left</h2>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
